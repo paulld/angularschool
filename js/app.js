b99b0ad8ca5a13477f92b491545bbbe234f919be
Paul <paul.lugagnedelpon@gmail.com>
2014-06-13 18:43:33 +0800

    add tab controller

--- js/app.js
+++ js/app.js
@@ -5,6 +5,16 @@
     this.persons = persons;
   });
 
+  app.controller('TabController', function(){
+    this.tab = 1;
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
   var persons = [
     {
       name: "Bob",
