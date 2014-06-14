6566cb1ad85055c4f2c22f10ec42341523b78063
Paul <paul.lugagnedelpon@gmail.com>
2014-06-14 12:26:39 +0800

    add review form, new review is pushed but not persisted. fix tab controller

--- js/app.js
+++ js/app.js
@@ -6,22 +6,34 @@
   });
 
   app.controller('TabController', function(){
-    this.tab = 1;
-    this.selectTab = function(setTab){
-      this.tab = setTab;
+    this.currentTab = 1;
+    
+    this.selectTab = function(newTab){
+      this.currentTab = newTab;
     };
     this.isSelected = function(checkTab){
-      return this.tab === checkTab;
+      return this.currentTab === checkTab;
     };
   });
 
   app.controller('GalleryController', function(){
     this.current = 0;
     this.setCurrent = function(picture){
-      this.current = picture ? picture : 0;
+      this.current = picture ? picture : 0;           // this.current = newGallery || 0;
+    };
+    this.isCurrent = function(picture){
+      return this.current = picture;
     };
   });
 
+  app.controller('ReviewController', function(){
+    this.review = {};
+
+    this.addReview = function(person){
+      person.reviews.push(this.review);
+      this.review = {};      
+    };
+  });
 
   var persons = [
     {
@@ -35,13 +47,25 @@
       description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.",
       images: [
         {
-          full: 'http://lorempixel.com/g/300/300/business/1/',
-          thumb: 'http://lorempixel.com/g/96/96/business/1/'
+          full: '/assets/images/1-full.jpg',
+          thumb: '/assets/images/1-thumb.jpg'
         },
         {
-          full: 'http://lorempixel.com/g/300/300/business/2/',
-          thumb: 'http://lorempixel.com/g/96/96/business/2/'
+          full: '/assets/images/2-full.jpg',
+          thumb: '/assets/images/2-thumb.jpg'
         }
+      ],
+      reviews: [
+        {
+          stars: 2,
+          comment: "nice guy",
+          commentor: "John"
+        },
+        {
+          stars: 4,
+          comment: "great guy",
+          commentor: "Jimmy"
+        },
       ]
     },
     {
@@ -55,9 +79,21 @@
       description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
       images: [
         {
-          full: 'http://lorempixel.com/g/300/300/business/4/',
-          thumb: 'http://lorempixel.com/g/96/96/business/4/'
+          full: '/assets/images/3-full.jpg',
+          thumb: '/assets/images/3-thumb.jpg'
         }
+      ],
+      reviews: [
+        {
+          stars: 5,
+          comment: "Great!",
+          commentor: "John"
+        },
+        {
+          stars: 4,
+          comment: "great guy",
+          commentor: "Jimmy"
+        },
       ]
     },
     {
@@ -71,17 +107,29 @@
       description: "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles. Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It va esser tam simplic quam Occidental in fact, it va esser Occidental. A un Angleso it va semblar un simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.",
       images: [
         {
-          full: 'http://lorempixel.com/g/300/300/business/7/',
-          thumb: 'http://lorempixel.com/g/96/96/business/7/'
+          full: '/assets/images/4-full.jpg',
+          thumb: '/assets/images/4-thumb.jpg'
         },
         {
-          full: 'http://lorempixel.com/g/300/300/business/8/',
-          thumb: 'http://lorempixel.com/g/96/96/business/8/'
+          full: '/assets/images/5-full.jpg',
+          thumb: '/assets/images/5-thumb.jpg'
         },
         {
-          full: 'http://lorempixel.com/g/300/300/business/9/',
-          thumb: 'http://lorempixel.com/g/96/96/business/9/'
+          full: '/assets/images/6-full.jpg',
+          thumb: '/assets/images/6-thumb.jpg'
         }
+      ],
+      reviews: [
+        {
+          stars: 1,
+          comment: "Should do better",
+          commentor: "John"  
+        },
+        {
+          stars: 2,
+          comment: "Fire him!",
+          commentor: "Jimmy"
+        },
       ]
     },
     {
@@ -95,20 +143,20 @@
       description: "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me",
       images: [
         {
-          full: 'http://lorempixel.com/g/300/300/business/10/',
-          thumb: 'http://lorempixel.com/g/96/96/business/10/'
+          full: '/assets/images/7-full.jpg',
+          thumb: '/assets/images/7-thumb.jpg'
         },
         {
-          full: 'http://lorempixel.com/g/300/300/business/5/',
-          thumb: 'http://lorempixel.com/g/96/96/business/5/'
+          full: '/assets/images/8-full.jpg',
+          thumb: '/assets/images/8-thumb.jpg'
         },
         {
-          full: 'http://lorempixel.com/g/300/300/business/6/',
-          thumb: 'http://lorempixel.com/g/96/96/business/6/'
+          full: '/assets/images/9-full.jpg',
+          thumb: '/assets/images/9-thumb.jpg'
         },
         {
-          full: 'http://lorempixel.com/g/300/300/business/3/',
-          thumb: 'http://lorempixel.com/g/96/96/business/3/'
+          full: '/assets/images/10-full.jpg',
+          thumb: '/assets/images/10-thumb.jpg'
         }
       ]
     },
