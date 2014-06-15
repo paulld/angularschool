94d8f6d4c812cd760ffed0f4d238a0060cfe91f4
Paul <paul.lugagnedelpon@gmail.com>
2014-06-15 12:59:23 +0800

    moved person controllers adn directives to a separate module people-person, and add that dependency to the people module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('people', []);
+  var app = angular.module('people', ['people-person']);
 
 // CONTROLLERS:
   app.controller('PeopleController', function(){
@@ -15,61 +15,6 @@
     this.isSelected = function(checkTab){
       return this.currentTab === checkTab;
     };
-  });
-
-// DIRECTIVES:    (NB: could use ng-include="'templates/person-details.html'" instead of the directive element)
-  app.directive('personDetails', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/person-details.html'
-    }
-  });
-
-  app.directive('personDescription', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/person-description.html'
-    }
-  });
-
-  app.directive('personGallery', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/person-gallery.html',
-      controller: function(){                         // NB: Directive includes a Controller and its alias
-        this.current = 0;
-        this.setCurrent = function(picture){
-          this.current = picture ? picture : 0;           // this.current = newGallery || 0;
-        };
-        this.isCurrent = function(picture){
-          return this.current = picture;
-        };
-      },
-      controllerAs: 'gallery'
-    }
-  });
-
-  app.directive('personReviews', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/person-reviews.html'
-    }
-  });
-
-  app.directive('personNewReview', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/person-new-review.html',
-      controller: function(){                           // NB: Directive includes a Controller and its alias
-        this.review = {};
-        this.addReview = function(person){
-          this.review.createdOn = Date.now();
-          person.reviews.push(this.review);
-          this.review = {};      
-        };
-      },
-      controllerAs: 'ReviewCtrl'
-    }
   });
 
 // MODEL:
